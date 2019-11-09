e300bc2bb8388a087eb0e5bd8d71ac44eb9afc08
Samuel-Nicolai <nicolaisamuel3@gmail.com>
2019-11-09 16:38:52 -0300

    correção da query

--- src/controllers/books.js
+++ src/controllers/books.js
@@ -3,16 +3,6 @@ const db = require('../../config/db')
 const controllerBooks = {
     async getAleatoryBooks(res) {
         const aleatoryBooks = await db('bookdescriptions')
-            .join(
-                'bookauthorsbooks',
-                'bookdescriptions.ISBN',
-                'bookauthorsbooks.ISBN'
-            )
-            .join(
-                'bookauthors',
-                'bookauthorsbooks.AuthorID',
-                'bookauthors.AuthorID',
-            )
             .orderByRaw('RAND()')
             .limit(4)
         res.json(aleatoryBooks)
