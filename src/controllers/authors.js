18ab71384d0b972c9d92b3493b23082af379771a
Samuel-Nicolai <nicolaisamuel3@gmail.com>
2019-11-25 19:56:07 -0300

    fix authors logs

--- src/controllers/authors.js
+++ src/controllers/authors.js
@@ -2,22 +2,16 @@ const db = require('../../config/db')
 
 const controllerAuthors = {
     async getAuthorsOfBook(id, res) {
-        if (id) {
-            const authors = await db('bookauthors')
-                .join(
-                    'bookauthorsbooks',
-                    'bookauthors.AuthorID',
-                    'bookauthorsbooks.AuthorID'
-                )
-                .where({
-                    ISBN: id
-                })
-            if (!authors) {
-                res.sendStatus(404);
-            }
-            res.json(authors);
-        }
-        res.sendStatus(404);
+        const authors = await db('bookauthors')
+            .join(
+                'bookauthorsbooks',
+                'bookauthors.AuthorID',
+                'bookauthorsbooks.AuthorID'
+            )
+            .where({
+                ISBN: id
+            })
+        res.json(authors);
     },
     async getBooksOfAuthor(id, res) {
         const books = await db('bookdescriptions')
